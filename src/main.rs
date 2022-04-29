94e9bfc90d2174e5d1c67dbb4fc3f6b0f35e898a
kit <kit@hastur.io>
2022-04-29 11:19:10 +1000

    refactor: parse arguments explicitly
    It's expected that in the future *rustdown* will accept multiple
    `--manifest` arguments so that a cache can be built with various
    compatible release channels. Clap has experimental support for argument
    groups but this is not exposed with the Parser that was used previously.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ mod extension;
 
 use cache::Cache;
 use channel::{manifest::Manifest, Channel};
-use clap::Parser;
+use clap::{Arg, Command};
 use download::Downloader;
 use eyre::{eyre, Result, WrapErr};
-use std::{num::NonZeroUsize, path::PathBuf};
+use std::{env, num::NonZeroUsize, path::PathBuf, str::FromStr};
 use tokio::{fs::File, io::AsyncReadExt};
 use tracing::{info, Level};
 use url::Url;
@@ -24,48 +24,119 @@ async fn synchronise(cache: &Cache, jobs: NonZeroUsize) -> Result<()> {
     Ok(())
 }
 
-/// Collects the program arguments.
-#[derive(Parser, Debug)]
-#[clap(version, about)]
+#[derive(Debug)]
 struct Arguments {
-    /// The path of the cache.
     path: PathBuf,
-
-    /// The URL describing where the cache will be hosted.
-    ///
-    /// The file system location of the cache will be used when this argument is not provided.
-    #[clap(long)]
-    host: Option<Url>,
-
-    /// The path to the channel manifest.
-    #[clap(short, long)]
+    host: Url,
     manifest: PathBuf,
-
-    /// The channel that the manifest is for.
-    #[clap(short, long)]
     channel: Channel,
-
-    /// The number of jobs that can run in parallel.
-    #[clap(short, long, default_value_t = NonZeroUsize::new(1).unwrap())]
     jobs: NonZeroUsize,
-
-    /// The log level.
-    #[clap(short, long, default_value_t = Level::INFO)]
     log_level: Level,
+}
+
+#[derive(Debug)]
+struct Parser {
+    command: Command<'static>,
+}
+
+impl Parser {
+    fn new() -> Self {
+        let command = Command::new(env!("CARGO_PKG_NAME"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(env!("CARGO_PKG_DESCRIPTION"))
+            .arg(
+                Arg::new("path")
+                    .takes_value(true)
+                    .validator(|p| Url::from_directory_path(p).map_err(|_| "invalid path"))
+                    .required(true)
+                    .help("The path of the cache"),
+            )
+            .arg(
+                Arg::new("host")
+                    .long("host")
+                    .takes_value(true)
+                    .validator(Url::parse)
+                    .help("The URL describing where the cache will be hosted")
+                    .long_help("The URL describing where the cache will be hosted. The file system location will be used when this argument is not provided."),
+            )
+            .arg(
+                Arg::new("manifest")
+                    .short('m')
+                    .long("manifest")
+                    .takes_value(true)
+                    .required(true)
+                    .help("The path to the channel manifest"),
+            )
+            .arg(
+                Arg::new("channel")
+                    .short('c')
+                    .long("channel")
+                    .takes_value(true)
+                    .validator(Channel::from_str)
+                    .help("The channel that the manifest is for"),
+            )
+            .arg(
+                Arg::new("jobs")
+                    .short('j')
+                    .long("jobs")
+                    .takes_value(true)
+                    .validator(NonZeroUsize::from_str)
+                    .help("The number of jobs that can run in parallel"),
+            )
+            .arg(
+                Arg::new("log_level")
+                    .short('l')
+                    .long("log-level")
+                    .takes_value(true)
+                    .possible_values(["trace", "debug", "info", "warn", "error"])
+                    .default_value("info")
+                    .help("The log level"),
+            );
+
+        Self { command }
+    }
+
+    fn parse(&mut self, arguments: impl Iterator<Item = String>) -> Result<Arguments, clap::Error> {
+        let matches = self.command.try_get_matches_from_mut(arguments)?;
+
+        let path = PathBuf::from(matches.value_of("path").expect("missing path"));
+        let host = match matches.value_of("host") {
+            Some(host) => Url::parse(host).expect("invalid host"),
+            None => Url::from_directory_path(&path).expect("invalid path"),
+        };
+
+        let manifest = PathBuf::from(matches.value_of("manifest").expect("missing manifest"));
+        let channel = Channel::from_str(matches.value_of("channel").expect("missing channel"))
+            .expect("invalid channel");
+
+        let jobs = NonZeroUsize::from_str(matches.value_of("jobs").expect("missing jobs"))
+            .expect("invalid jobs");
+
+        let log_level = Level::from_str(matches.value_of("log_level").expect("missing log level"))
+            .expect("invalid log level");
+
+        Ok(Arguments {
+            path,
+            host,
+            manifest,
+            channel,
+            jobs,
+            log_level,
+        })
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let arguments = Arguments::parse();
+    let arguments = Parser::new()
+        .parse(env::args().into_iter())
+        .map_err(|error| error.exit())
+        .expect("unhandled error");
 
     tracing_subscriber::fmt()
         .with_max_level(arguments.log_level)
         .init();
-
-    let host = arguments.host.unwrap_or(
-        Url::from_directory_path(&arguments.path)
-            .map_err(|_| eyre!("failed to convert the path to a url"))?,
-    );
 
     let mut bytes = Vec::new();
     let mut file = File::open(arguments.manifest)
@@ -79,6 +150,6 @@ async fn main() -> Result<()> {
     let manifest =
         Manifest::from_slice(bytes.as_slice()).wrap_err(eyre!("failed to deserialise manifest"))?;
 
-    let cache = Cache::new(arguments.path, manifest, arguments.channel, host);
+    let cache = Cache::new(arguments.path, manifest, arguments.channel, arguments.host);
     synchronise(&cache, arguments.jobs).await
 }
